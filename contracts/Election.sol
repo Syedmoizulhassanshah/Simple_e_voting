1c0cdc6465b03e35dd6f0db339132679cd337fcd
Syedmoizulhassanshah <syedmoizulhassanshah@gmail.com>
2021-11-04 11:48:23 +0500

    Simple voting

---
+++ contracts/Election.sol
@@ -0,0 +1,73 @@
+pragma solidity 0.4.25;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+          uint id;
+        string name;
+        string partyname;
+        uint voteCount;
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+       addCandidate("Candidate 1","party1");
+        addCandidate("Candidate 2","party2");
+        
+    }
+
+    function addCandidate (string _name,string _partyname) public {
+        candidatesCount ++;
+       candidates[candidatesCount] = Candidate(candidatesCount, _name, _partyname, 0);
+    }
+
+
+
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+
+/// generating results
+             function getWinner () public view returns(string winner){
+        uint maxNumberOfVotes;
+      //  string winner;
+        //uint length = candidates.length;
+        for(uint i=0; i<=candidatesCount; i++){
+            if(candidates[i].voteCount> maxNumberOfVotes){
+                maxNumberOfVotes=candidates[i].voteCount;
+                //winner =i;
+                winner = candidates[i].name;
+                
+            }
+        }
+        
+    }
+
+
+}
